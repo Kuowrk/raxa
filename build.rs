8874d04f3089d8303c5fc433464d084270251eb4
Khoa Hoang <koadahoakodah@gmail.com>
2025-01-20 00:17:31 -0500

    Use naga only for wgsl and add shaderc for glsl

--- build.rs
+++ build.rs
@@ -1,9 +1,11 @@
+extern crate shaderc;
+
 use color_eyre::Result;
 use color_eyre::eyre::OptionExt;
+use color_eyre::eyre::eyre;
 use naga::{
-    back::spv, front::glsl::{Frontend, Options},
-    valid::{Capabilities, ValidationFlags, Validator},
-    ShaderStage
+    back::spv, front::wgsl,
+    valid::{Capabilities, ValidationFlags, Validator}
 };
 use std::{env, fs, path::Path};
 
@@ -13,6 +15,11 @@ fn main() -> Result<()> {
     compile_shaders()?;
 
     Ok(())
+}
+
+enum ShaderLanguage {
+    Glsl,
+    Wgsl,
 }
 
 fn compile_shaders() -> Result<()> {
@@ -28,29 +35,20 @@ fn compile_shaders() -> Result<()> {
             .extension()
             .and_then(|ext| ext.to_str())
             .ok_or_eyre(format!("Shader file has no extension: {:?}", path))?;
-        let shader_stage = match ext {
-            "vert" => ShaderStage::Vertex,
-            "frag" => ShaderStage::Fragment,
-            "comp" => ShaderStage::Compute,
-            _ => {
-                log::warn!("Skipping non-GLSL file: {:?}", path);
-                continue;
-            }
+
+        let shader_lang = match ext {
+            "vert" => ShaderLanguage::Glsl,
+            "frag" => ShaderLanguage::Glsl,
+            "comp" => ShaderLanguage::Glsl,
+            "wgsl" => ShaderLanguage::Wgsl,
+            _ => return Err(eyre!("Shader language not recognized for file: {:?}", path)),
         };
 
-        // Read the GLSL file and parse into IR
-        let source = fs::read_to_string(&path)?;
-        let mut frontend = Frontend::default();
-        let module = frontend.parse(&Options::from(shader_stage), &source)?;
+        let spv_binary = match shader_lang {
+            ShaderLanguage::Glsl => compile_glsl(&path)?,
+            ShaderLanguage::Wgsl => compile_wgsl(&path)?,
+        };
         
-        // Validate the IR
-        let mut validator = Validator::new(ValidationFlags::all(), Capabilities::all());
-        let validation_info = validator.validate(&module)?;
-        log::info!("{:?}", validation_info);
-
-        // Generate the SPIR-V binary
-        let spv_binary = spv::write_vec(&module, &validation_info, &spv::Options::default(), None)?;
-
         // Write the SPIR-V binary to a file
         let shader_name = path
             .file_stem()
@@ -64,4 +62,33 @@ fn compile_shaders() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn compile_glsl(filepath: &Path) -> Result<Vec<u32>> {
+    Err(eyre!("GLSL not supported yet"))
+    /*
+    let shader_stage = match ext {
+        "vert" => ShaderStage::Vertex,
+        "frag" => ShaderStage::Fragment,
+        "comp" => ShaderStage::Compute,
+        _ => {
+            log::warn!("Skipping non-GLSL file: {:?}", path);
+            return;
+        }
+    };
+    */
+}
+
+fn compile_wgsl(filepath: &Path) -> Result<Vec<u32>> {
+    // Read the WGSL file and parse into IR
+    let source = fs::read_to_string(&filepath)?;
+    let module = wgsl::parse_str(&source)?;
+    
+    // Validate the IR
+    let mut validator = Validator::new(ValidationFlags::all(), Capabilities::all());
+    let validation_info = validator.validate(&module)?;
+    log::info!("{:?}", validation_info);
+
+    // Generate the SPIR-V binary
+    Ok(spv::write_vec(&module, &validation_info, &spv::Options::default(), None)?)
 }
